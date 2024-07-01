59d2282b2eb8267f98eea73abcb1813afe0808b3
Charles <charles4543@gmail.com>
2024-07-01 19:32:47 +0900

    win

--- process_data/WashData.py
+++ process_data/WashData.py
@@ -16,32 +16,10 @@ def wash_data(name, type):
         np.savetxt(output_file_path, data, delimiter=',', fmt='%f')
     return data
 
-wash_data("gashi","AC")
-wash_data("gashi","AD")
-wash_data("gashi","BC")
-wash_data("gashi","BD")
+names = ["gashi", "jingchen", "liu", "qing", "wang", "zhou"]
+types = ["AC", "AD", "BC", "BD"]
 
-wash_data("jingchen","AC")
-wash_data("jingchen","AD")
-wash_data("jingchen","BC")
-wash_data("jingchen","BD")
-
-wash_data("liu","AC")
-wash_data("liu","AD")
-wash_data("liu","BC")
-wash_data("liu","BD")
-
-wash_data("qing","AC")
-wash_data("qing","AD")
-wash_data("qing","BC")
-wash_data("qing","BD")
-
-wash_data("wang","AC")
-wash_data("wang","AD")
-wash_data("wang","BC")
-wash_data("wang","BD")
-
-wash_data("zhou","AC")
-wash_data("zhou","AD")
-wash_data("zhou","BC")
-wash_data("zhou","BD")
+# Call the function for each combination of name and type
+for name in names:
+    for type in types:
+        wash_data(name, type)
