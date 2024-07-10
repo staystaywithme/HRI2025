4d9477f50c05ee5b49e5b956b093131801983e72
staystaywithme <ryuusyunsei@gmail.com>
2024-07-10 17:44:38 +0900

    mac

--- process_data/gomi/reduce0.py
+++ process_data/gomi/reduce0.py
@@ -1,17 +1,17 @@
 import numpy as np
 import os
+import pandas as pd
 
 def wash_data(name, type):
     data = []
     for i in range(1,11):
-        file_path = f"C:\\Github_LIU\\SII2025\\Raw_Data\\{name}\\{name}_{type}{i:02}_washed.csv"
+        file_path = f"\\Users\\syunsei\\Desktop\\SII2025\\process_data\\ごみ\\{name}_{type}{i:02}_washed.csv"
         if not os.path.exists(file_path):
             print(f"File {file_path} does not exist, skipping.")
             continue
         data = np.loadtxt(file_path, delimiter=',', skiprows=1)
-        data = data[~(data > 1000).any(axis=1)]
-        data = data[~(data < -1000).any(axis=1)]
-        output_dir = "C:\\Github_LIU\\SII2025\\process_data"
+        data = data[data.iloc[:, 4] != -0.0625]
+        output_dir = "\\Users\\syunsei\\Desktop\\SII2025\\process_data\\ごみ"
         output_file_path = os.path.join(output_dir, f"{name}_{type}{i:02}_washed.csv")
         np.savetxt(output_file_path, data, delimiter=',', fmt='%f')
     return data
