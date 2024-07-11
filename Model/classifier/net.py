3fb6bb8acc3a1db69c6d0f2583fd46bd47eb78a9
Charles <charles4543@gmail.com>
2024-07-11 12:57:22 +0900

    win

--- Model/classifier/net.py
+++ Model/classifier/net.py
@@ -1,20 +1,20 @@
 import torch
 import torch.nn as nn
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-class LSTMNetwork(nn.Module):
+class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(LSTMNetwork, self).__init__()
+        super(LSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
-        
+        self.output_layer = nn.Linear(hidden_size, num_classes)
+        #self.relu = nn.ReLU()
+
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        
-        out, _ = self.lstm(x, (h0, c0))
-        out = self.fc(out[:, -1, :])
+        hidden_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        cell_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        out, _ = self.lstm(x, (hidden_state, cell_state))
+        out = self.output_layer(out[:, -1, :])
         return out
