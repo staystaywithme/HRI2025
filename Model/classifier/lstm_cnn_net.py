e6ac29136e764026e998b3e4a8b470121ac702a9
Charles <charles4543@gmail.com>
2024-07-17 17:30:17 +0900

    win

--- Model/classifier/lstm_cnn_net.py
+++ Model/classifier/lstm_cnn_net.py
@@ -6,11 +6,6 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 class lstm_cnn(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(lstm_cnn, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.output_layer = nn.Linear(hidden_size, num_classes)
-        
         self.conv1 = nn.Conv1d(6, 64, 9)
         self.conv2 = nn.Conv1d(64, 32, 5)
         self.conv3 = nn.Conv1d(32, 32, 5)
@@ -22,14 +17,15 @@ class lstm_cnn(nn.Module):
         self.fc2 = nn.Linear(256, 108)  # 新增一个全连接层
         self.dropout = nn.Dropout(0.5)  # Dropout 层，概率为0.5
         self.fc3 = nn.Linear(108, 4)  # 这是原来的第二个全连接层，现在变成第三个
+        
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.output_layer = nn.Linear(hidden_size, num_classes)
+        
 
     def forward(self, x):
-        hidden_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-        cell_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-        out, _ = self.lstm(x, (hidden_state, cell_state))
-        out = self.output_layer(out[:, -1, :])
-        
-        #x = x.permute(0,2,1)
+        x = x.permute(0,2,1)
         x = self.relu(self.conv1(x))
         x = self.maxpool(x)
         x = self.relu(self.conv2(x))
@@ -44,5 +40,11 @@ class lstm_cnn(nn.Module):
         x = self.relu(self.fc2(x))
         x = self.dropout(x)
         x = self.fc3(x)
+        
+        hidden_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        cell_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        out, _ = self.lstm(x, (hidden_state, cell_state))
+        out = self.output_layer(out[:, -1, :])
+        
         return x
         #return out
