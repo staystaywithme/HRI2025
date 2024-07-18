bdbcf15ada9e403924939369d8d42cfd07e2986e
staystaywithme <ryuusyunsei@gmail.com>
2024-07-18 15:13:32 +0900

    mac

---
+++ FeedbackSys/lstm_cnn_net.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class lstm_cnn(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(lstm_cnn, self).__init__()
+        self.conv1 = nn.Conv1d(12, 64, 9)
+        self.conv2 = nn.Conv1d(64, 32, 5)
+        self.conv3 = nn.Conv1d(32, 32, 5)
+        self.conv4 = nn.Conv1d(32, 16, 5)
+        self.relu = nn.LeakyReLU()
+        self.maxpool = nn.MaxPool1d(2, 2, padding=1)  # 添加池化层
+
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(16, hidden_size, num_layers, batch_first=True)  # 修改 input_size 为最后一个 Conv1d 层的输出通道数
+        self.output_layer = nn.Linear(hidden_size, num_classes)
+        
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)  # 将形状调整为 (batch, feature, seq)
+        x = self.relu(self.conv1(x))
+        x = self.maxpool(x)
+        x = self.relu(self.conv2(x))
+        x = self.maxpool(x)
+        x = self.relu(self.conv3(x))
+        x = self.maxpool(x)
+        x = self.relu(self.conv4(x))
+        x = self.maxpool(x)
+
+        x = x.permute(0, 2, 1)  # 将形状调整为 (batch, seq, feature) 以输入 LSTM
+
+        hidden_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        cell_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        out, _ = self.lstm(x, (hidden_state, cell_state))
+        out = self.output_layer(out[:, -1, :])
+
+        return out
